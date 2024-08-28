53c4090e71ba3f7bcb6b682f21cd99d0f48d8744
ELCEBOLLAZ <sebastorito123@gmail.com>
2024-08-28 11:36:52 -0600

    Add files via upload

---
+++ EVA1_4_TELEVISION/src/eva1_4_television/EVA1_4_TELEVISION.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_4_television;
+
+/**
+ *
+ * @author samue
+ */
+public class EVA1_4_TELEVISION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String marca;
+        String calidad;
+        String pantalla;
+        int pulgadas;
+        double precio;
+        
+        //ASIGNAR VALORES
+        marca = "Samsung";
+        calidad = "8K";
+        pantalla = "QLED"; 
+        pulgadas = 85;
+        precio = 10816.58;
+        
+        System.out.println(marca);  //Imprimir el contenido de la variable
+        System.out.println(calidad);
+        System.out.println(pantalla);
+        System.out.println(pulgadas);
+        System.out.println(precio);
+    }
+    
+}
---
+++ EVA_2_VARIABLES/src/eva_2_variables/EVA_2_VARIABLES.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_variables;
+
+/**
+ *
+ * @author samue
+ */
+public class EVA_2_VARIABLES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Todo el codigo va aqui adentro
+        System.out.println("HOLA MUNDO!!");  //Hola mundo es una cadena de texto
+        System.out.println(100);             //100 es un numero entero
+        System.out.println('c');             //c es un caracter
+        System.out.println(true);            //Valores logicos
+        System.out.println(false);
+        System.out.println(1000.52);         //Numero con fraccion
+        
+        //Tipos de datos
+        //ENTEROS
+        //int
+        //long
+        //byte
+        //short
+        //PUNTO FLOTANTE
+        //double
+        //float
+        //TEXTO
+        //String
+        //CARACTER
+        //Char
+        //LOGICOS (true, false)
+        
+        int edad; //Declarando una variable de tipo entero llamada
+                  //edad
+        double edad2; //NO PODEMOS TENER 2 VARIABLES CON EL MISMO NOMBRE
+        String nombre;
+        String apellidos;
+        double promedio;
+        
+        edad = 18;    //Estoy asignando el valor 18 dentro de la
+                      //Variable edad
+        
+        System.out.println(edad);
+    }
+    
+    
+}
